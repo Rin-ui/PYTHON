1af254f41e206a32d38b8011d27d961dc3e9bb4b
Rin-ui <rajashri.biology@gmail.com>
2025-01-15 12:06:43 +0530

    Create2Metrices.py

---
+++ Create2Metrices.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Creating two 2x3 matrices
+matrix1 = np.array([[1, 2, 3],
+                    [4, 5, 6]])
+
+matrix2 = np.array([[7, 8, 9],
+                    [10, 11, 12]])
+
+# Print the matrices
+print("Matrix 1:")
+print(matrix1)
+
+print("\nMatrix 2:")
+print(matrix2)
